29b7666a8febcb282509100ff20a08938b57dd09
Ali Eren <alierenguner@hotmail.com>
2024-03-07 22:38:11 +0300

    Client-Feature: Create Builder.ElementList.(Container, Title, Element) Components

---
+++ packages/ui-builder/src/components/ElementList.tsx
@@ -0,0 +1,43 @@
+interface ListContainerProps {
+    children?: React.ReactElement<ListTitleProps | ListElementProps> | React.ReactElement<ListTitleProps | ListElementProps>[]
+}
+
+interface ListTitleProps {
+    title: string
+}
+
+interface ListElementProps {
+    title: string,
+    icon?: JSX.Element
+}
+
+const ListContainer: React.FunctionComponent<ListContainerProps> = (props) => {
+    return (
+        <div className="grid grid-cols-3 gap-2 w-full mt-2">
+            {props.children}
+        </div>
+    )
+}
+
+const ListTitle: React.FunctionComponent<ListTitleProps> = (props) => {
+    return (
+        <h2 className="col-span-3 pl-1 text-xs font-semibold">{props.title}</h2>
+    );
+}
+
+const ListElement: React.FunctionComponent<ListElementProps> = (props) => {
+    return (
+        <div className="flex flex-col items-center justify-center w-auto h-24 border border-solid rounded-md border-slate-800 cursor-pointer">
+            {props.icon}
+            <span className="text-center text-xs mt-2">{props.title}</span>
+        </div>
+    );
+}
+
+const ElementList = {
+    Container: ListContainer,
+    Title: ListTitle,
+    Element: ListElement
+}
+
+export default ElementList;
--- packages/ui-builder/src/views/ElementView.tsx
+++ packages/ui-builder/src/views/ElementView.tsx
@@ -1,3 +1,7 @@
+import { Fragment } from "react";
+import { HtmlCollection } from "../dummy-data/html-collection";
+// Components
+import ElementList from "../components/ElementList";
 import ElementSearchBox from "../components/ElementSearchBox";
 import Panel from "../components/Panel";
 
@@ -5,6 +9,22 @@ export default function ElementView() {
     return (
         <Panel title="Elements">
             <ElementSearchBox />
+
+            <ElementList.Container>
+                {
+                    [HtmlCollection].map((collection) => (
+                        <Fragment key={collection.id}>
+                            <ElementList.Title title={collection.name} />
+
+                            {
+                                collection.elements.map((collectionElement) => (
+                                    <ElementList.Element key={collectionElement.id} title={collectionElement.name} icon={collectionElement.icon} />
+                                ))
+                            }
+                        </Fragment>
+                    ))
+                }
+            </ElementList.Container>
         </Panel>
     )
 }
