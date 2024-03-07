84065026980645c37a72df2b7ab007a5f56802f3
Ali Eren <alierenguner@hotmail.com>
2024-03-07 21:16:13 +0300

    Client-Feature: Create Builder.ExplorerView Component

---
+++ packages/ui-builder/src/views/ExplorerView.tsx
@@ -0,0 +1,12 @@
+export default function ExplorerView() {
+    return (
+        <div className="flex-1 flex flex-col w-full h-full">
+            <div className="flex items-center w-full h-10 bg-blue-700">
+                <h2 className="ml-2 font-semibold">Elements</h2>
+            </div>
+            <div className="flex flex-col p-2">
+                
+            </div>
+        </div>
+    )
+}
