bea51bed2698e1be837b3fb6d6f7d0a15ba43ee5
Ali Eren <alierenguner@hotmail.com>
2024-03-07 21:14:58 +0300

    Client-Feature: Create Builder.ElementView Component

--- packages/ui-builder/src/containers/DeveloperContainer.tsx
+++ packages/ui-builder/src/containers/DeveloperContainer.tsx
@@ -1,9 +1,11 @@
+import ElementView from "../views/ElementView";
+
 export default function DeveloperContainer() {
     return (
         <div className="flex-1 flex w-full h-full">
             {/* Left Section */}
             <div className="flex flex-col w-[280px] border-r border-solid border-neutral-900">
-                
+                <ElementView />
             </div>
             {/* Middle Section */}
             <div className="flex-1 flex flex-col justify-center p-[50px]">
